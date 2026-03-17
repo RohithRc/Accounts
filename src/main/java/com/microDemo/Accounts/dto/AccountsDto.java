478d18ef585e54c86870609cf571165a74121968
RohithRc <rohithchandra2000@gmail.com>
2026-03-16 22:24:48 -0500

    Added Dto's and Repositories

---
+++ src/main/java/com/microDemo/Accounts/dto/AccountsDto.java
@@ -0,0 +1,11 @@
+package com.microDemo.Accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+    private Long accountNumber;
+    private String accountType;
+    private String branchAddress;
+    private String mobileNumber;
+}
