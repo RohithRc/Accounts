019f82efde3bcc1e81d3b101179dad50e7265037
RohithRc <rohithchandra2000@gmail.com>
2026-03-17 22:22:02 -0500

    Added Validations

--- src/main/java/com/microDemo/Accounts/dto/AccountsDto.java
+++ src/main/java/com/microDemo/Accounts/dto/AccountsDto.java
@@ -1,10 +1,14 @@
 package com.microDemo.Accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
     private Long accountNumber;
+    @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 }
